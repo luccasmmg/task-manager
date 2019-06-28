552bdd2c03d5fb88cb04967fa90bbedb45479d80
Luccas Mateus <luccasmmg@gmail.com>
2019-06-28 10:34:32 -0300

    Finished file uploading

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,21 +2,9 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
+const sharp = require('sharp');
 
 const router = new express.Router();
-const upload = multer({
-  dest: 'avatar',
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error('File must be a Word Document'));
-    }
-
-    cb(undefined, true);
-  }
-});
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -113,8 +101,56 @@ router.delete('/users/me', auth, async (req, res) => {
   }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
-  res.send();
+const upload = multer({
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('File must be an image'));
+    }
+
+    return cb(undefined, true);
+  }
+});
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,
+  res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250}).png().toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  }, (error, req, res, next) => {
+    res.status(400)
+      .send({
+        error: error.message
+      });
+  });
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined;
+  try {
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(400)
+      .send();
+  }
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set('Content-Type', 'image/png');
+    res.send(user.avatar);
+  } catch(e) {
+    res.status(404).send();
+  }
 });
 
 module.exports = router;
